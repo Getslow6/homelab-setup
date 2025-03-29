08db124c2abbd8c346dc641ecf89e4c97a599577
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-03-30 00:57:08 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,5 +7,11 @@ echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 # https://community-scripts.github.io/ProxmoxVE/scripts?id=komodo
 bash -c "$(wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/ct/komodo.sh)"
 
+# Get the container ID of the container with the name komodo
+CTID=$(pct list | grep komodo | cut -d " " -f 1)
+
+# Rename the hostname of the just created container to 'Dockerhost'
+pct set $CTID --hostname dockerhost
+
 # Run additional code inside the just created LXC container
 lxc-attach -n "$CTID" -- bash -c "$(wget -qLO - https://github.com/Getslow6/homelab-setup/raw/main/setup-part2.sh)"
