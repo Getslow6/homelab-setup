18d7c004612b5978966b05e5fdb12901f8f01a11
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-03-29 22:19:59 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,5 +8,4 @@ echo "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
 bash -c "$(wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/ct/komodo.sh)"
 
 # Run additional code inside the just created LXC container
-hostname
-#lxc-attach -n "$CTID" -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/install/$var_install.sh)"
+lxc-attach -n "$CTID" -- bash -c "$(wget -qLO - https://github.com/Getslow6/homelab-setup/raw/main/setup-part2.sh)"
