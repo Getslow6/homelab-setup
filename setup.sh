71c81c645b57b71a9e5eb56e9e6098125178257e
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-03-30 01:15:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,5 +13,14 @@ CTID=$(pct list | grep komodo | cut -d " " -f 1)
 # Rename the hostname of the just created container to 'Dockerhost'
 pct set $CTID --hostname dockerhost
 
+# Reboot to finalise changing the hostname
+pct reboot $CTID
+echo "Rebooting container, waiting for restart..."
+sleep 2
+while [[ "$(pct status "$CTID" | awk '{print $2}')" != "running" ]]; do
+    sleep 2
+done
+echo "Containter has restarted"
+
 # Run additional code inside the just created LXC container
 lxc-attach -n "$CTID" -- bash -c "$(wget -qLO - https://github.com/Getslow6/homelab-setup/raw/main/setup-part2.sh)"
