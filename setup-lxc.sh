5aaea2b76efb304d6d6fe646b2f35c1ef8904610
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-04-17 21:48:21 +0200

    Update setup-lxc.sh

--- setup-lxc.sh
+++ setup-lxc.sh
@@ -18,12 +18,14 @@ get_password() {
     local title="$2"
     whiptail --backtitle "Homelab setup" --passwordbox "\n$prompt" 9 58 --title "$title" 3>&1 1>&2 2>&3
 }
+
+# First install Git on Alpine Linux
+apk add git -q
+
 # Get GitHub repository details from the user
 GITHUB_REPOSITORY=$(get_input    "Enter your GitHub repository"                     "GitHub repository" "https://github.com/Getslow6/homelab-config") || error_exit "Failed to get GitHub repository"
 GITHUB_USER=$(      get_input    "Enter your GitHub username for committing to Git" "Git Username")     "Getslow6" || error_exit "Failed to get GitHub username"
 GITHUB_PAT=$(       get_password "Enter your GitHub Personal Access Token (PAT)"    "GitHub PAT")        || error_exit "Failed to get GitHub PAT"
-
-
 
 # adduser $USER
 # chown $USER: /srv
@@ -32,35 +34,6 @@ git config --global credential.helper store
 # Make sure that local git uses the already stored credentials
 git config --global user.email "$GITHUB_USER@proxmox.local"
 git config --global user.name "$GITHUB_USER"
-
-# # We will generate an SSH key to authenticate with Github
-# # -f specifies where the SSH key is stored
-# # -N specifies the new passphrase
-# # -q makes the execution silent
-# # <<<y will answer 'yes' if there is already an SSH key in the target folder
-# KEY_LOCATION=$HOME/.ssh/id_ed25519
-# ssh-keygen -t ed25519 -C $GIT_MAIL -f $KEY_LOCATION -N $GIT_PASSPHRASE -q <<<y
-# clear
-# # If the passphrase is not empty
-# if [[ ! -z "$GIT_PASSPHRASE" ]]; then
-#   GIT_SAVEPASSPHRASE=$(whiptail --backtitle "Homelab setup" \
-#                             --yesno "\nDo do want to save the passhrase to the SSH agent, so it doesn't need entering everytime you want to access your repository?" 11 58 \
-#                             --title "Save SSH key passphrase?" \
-#                             3>&1 1>&2 2>&3)
-#   if $GIT_SAVEPASSPHRASE; then
-#     # Start an SSH agent
-#     eval "$(ssh-agent -s)"
-
-#     # Save the SSH key to the agent
-#     ssh-add $KEY_LOCATION
-#   fi
-# fi
-
-# echo ""
-# echo "An SSH key has been generated. This key needs to be added to your Github account. Copy the following line and add it to your account:"
-# echo ""
-# cat ~/.ssh/id_ed25519.pub
-# echo ""
 
 # Clone the github config to the /srv folder
 echo "▶️ Cloning Git repository"
