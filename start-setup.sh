bcf7660d9cc57406f4750a3107740a59d75f612a
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-04-17 21:38:13 +0200

    Update start-setup.sh

--- start-setup.sh
+++ start-setup.sh
@@ -14,8 +14,11 @@ get_input() {
     whiptail --backtitle "Homelab setup" --inputbox "\n$prompt" 9 58 "$default" --title "$title" 3>&1 1>&2 2>&3
 }
 
+# Get the LXC container config file that is edited the last and strip it from .conf
+CTID_DEFAULT=$(ls -Art  /etc/pve/lxc/ | tail -n 1 | sed 's/\.conf$//')
+
 # Get GitHub repository details from the user
-CTID=$(get_input "Enter the container ID of the container you just created" "Container ID") || error_exit "Failed to get Container ID"
+CTID=$(get_input "Enter the container ID of the container you just created" "Container ID" "$CTID_DEFAULT" ) || error_exit "Failed to get Container ID"
 
 # Run additional code inside the just created LXC container
 lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https://github.com/Getslow6/homelab-setup/raw/main/setup-lxc.sh)"
