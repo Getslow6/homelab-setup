5684af8fd0b1feaf979406069dec3d89abfa4c9a
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-04-18 08:40:34 +0000

    Changes to whiptail

--- setup-lxc.sh
+++ setup-lxc.sh
@@ -10,7 +10,7 @@ apk add git -q
 GITHUB_REPOSITORY=$(get_input    "Enter your GitHub repository"                     "GitHub repository" "Getslow6/homelab-config") || error_exit "Failed to get GitHub repository"
 GITHUB_USER=$(      get_input    "Enter your GitHub username for committing to Git" "Git Username"      "Getslow6" ) || error_exit "Failed to get GitHub username"
 GITHUB_PAT=$(       get_password "Enter your GitHub Personal Access Token (PAT)"    "GitHub PAT")        || error_exit "Failed to get GitHub PAT"
-
+clear
 # adduser $USER
 # chown $USER: /srv
 
@@ -22,16 +22,18 @@ git config --global user.name "$GITHUB_USER"
 # Clone the GitHub config to the /srv folder
 msg_info "Cloning Git repository"
 git clone --quiet https://${GITHUB_USER}:${GITHUB_PAT}@github.com/${GITHUB_REPOSITORY} /srv || error_exit "Failed cloning the repository"
-msg_ok "Cloning Git repository"
+msg_ok "Cloned Git repository"
 
 # Ensure local Git uses the stored credentials
 cd /srv || error_exit "Failed to change directory to /srv"
 git config credential.helper store
 
 # Create Docker networks
+msg_info "Creating Docker networks"
 docker network create mqtt > /dev/null 2>&1
 docker network create proxy > /dev/null 2>&1
 docker network create authelia > /dev/null 2>&1
+msg_ok "Created Docker networks"
 
 # Build the containers list from directories
 containerlist=""
@@ -65,13 +67,12 @@ readarray -t containers <<< "$(echo "$START_CONTAINERS" | tr -d '"')"
 for container in "${containers[@]}"; do
   compose_file="/srv/applications/$container/docker-compose.yml"
 
-  echo "▶️ Starting Docker Compose for: $container"
+  msg_info "Starting Docker Compose for: $container"
 
   if docker compose -f "$compose_file" up -d; then
-    echo "✅ Successfully started: $container"
+    msg_ok "Container '$container' started successfully"
   else
-    echo "❌ Failed to start: $container" >&2
+    msg_error "Failed to start: $container"
   fi
 done
 
-echo "✅ Setup complete"
