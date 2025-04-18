f78e82322530e139e750b5599b30b12af9f348f6
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-04-18 07:46:16 +0000

    Make bash compatible

--- setup-lxc.sh
+++ setup-lxc.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 
-# Load the self defined support functions
+# Load the self-defined support functions
 source <(curl -fsSL https://github.com/Getslow6/homelab-setup/raw/main/setup.func)
 
 # First install Git on Alpine Linux
@@ -18,21 +19,19 @@ git config --global credential.helper store
 git config --global user.email "$GITHUB_USER@proxmox.local"
 git config --global user.name "$GITHUB_USER"
 
-# Clone the github config to the /srv folder
+# Clone the GitHub config to the /srv folder
 echo "▶️ Cloning Git repository"
 git clone --quiet https://${GITHUB_USER}:${GITHUB_PAT}@github.com/${GITHUB_REPOSITORY} /srv || error_exit "Failed cloning the repository"
 echo "✅ Cloned Git repository"
 
-# Make sure that local git uses the already stored credentials
-cd /srv
+# Ensure local Git uses the stored credentials
+cd /srv || error_exit "Failed to change directory to /srv"
 git config credential.helper store
 
-
-# Create docker networks that are used
+# Create Docker networks
 docker network create mqtt
 docker network create proxy
 docker network create authelia
-
 
 # Build the containers list from directories
 containerlist=""
@@ -45,28 +44,23 @@ containers_on="\
  traefik\
 " 
 
-
 # Build the options string for whiptail
-options=""
-for container in $containers; do
+options=()
+for container in $containerlist; do
   if echo "$containers_on" | grep -qw "$container"; then
-    options="$options $container $container ON"
+    options+=("$container" "$container" "ON")
   else
-    options="$options $container $container OFF"
+    options+=("$container" "$container" "OFF")
   fi
 done
 
 # Show the checklist
-choices=$(whiptail --title "Select containers to start" \
-  --checklist "Choose containers:" 20 60 10 \
-  "$options" \
-  3>&1 1>&2 2>&3)
-echo "CHOICE:"
-echo "$choices"
+
+START_CONTAINERS=$(get_input    "Select containers to start" "Choose containers:" "${options[@]}") || error_exit "Failed to get select startup containers"
+
 
 # Convert the quoted string into an array
-# This safely splits on whitespace while respecting quotes
-read -r -a containers <<< "$choices"
+readarray -t containers <<< "$(echo "$START_CONTAINERS" | tr -d '"')"
 
 # Loop through each container and bring it up
 for container in "${containers[@]}"; do
