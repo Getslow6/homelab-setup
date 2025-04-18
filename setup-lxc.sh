f5c735658297401ca4347668ae56dc22f30d16d1
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-04-18 09:27:03 +0000

    temporary

--- setup-lxc.sh
+++ setup-lxc.sh
@@ -3,37 +3,37 @@
 # Load the self-defined support functions
 source <(curl -fsSL https://github.com/Getslow6/homelab-setup/raw/main/setup.func)
 
-# First install Git on Alpine Linux
-apk add git -q
+# # First install Git on Alpine Linux
+# apk add git -q
 
-# Get GitHub repository details from the user
-GITHUB_REPOSITORY=$(get_input    "Enter your GitHub repository"                     "GitHub repository" "Getslow6/homelab-config") || error_exit "Failed to get GitHub repository"
-GITHUB_USER=$(      get_input    "Enter your GitHub username for committing to Git" "Git Username"      "Getslow6" ) || error_exit "Failed to get GitHub username"
-GITHUB_PAT=$(       get_password "Enter your GitHub Personal Access Token (PAT)"    "GitHub PAT")        || error_exit "Failed to get GitHub PAT"
-clear
-# adduser $USER
-# chown $USER: /srv
+# # Get GitHub repository details from the user
+# GITHUB_REPOSITORY=$(get_input    "Enter your GitHub repository"                     "GitHub repository" "Getslow6/homelab-config") || error_exit "Failed to get GitHub repository"
+# GITHUB_USER=$(      get_input    "Enter your GitHub username for committing to Git" "Git Username"      "Getslow6" ) || error_exit "Failed to get GitHub username"
+# GITHUB_PAT=$(       get_password "Enter your GitHub Personal Access Token (PAT)"    "GitHub PAT")        || error_exit "Failed to get GitHub PAT"
+# clear
+# # adduser $USER
+# # chown $USER: /srv
 
-git config --global credential.helper store
-# Make sure that local git uses the already stored credentials
-git config --global user.email "$GITHUB_USER@proxmox.local"
-git config --global user.name "$GITHUB_USER"
+# git config --global credential.helper store
+# # Make sure that local git uses the already stored credentials
+# git config --global user.email "$GITHUB_USER@proxmox.local"
+# git config --global user.name "$GITHUB_USER"
 
-# Clone the GitHub config to the /srv folder
-msg_info "Cloning Git repository"
+# # Clone the GitHub config to the /srv folder
+# msg_info "Cloning Git repository"
 
-# Ensure folder is empty
-rm -rf /srv/.[!.]* 
-rm -rf /srv/*
+# # Ensure folder is empty
+# rm -rf /srv/.[!.]* 
+# rm -rf /srv/*
 
-git clone --quiet https://${GITHUB_USER}:${GITHUB_PAT}@github.com/${GITHUB_REPOSITORY} /srv || error_exit "Failed cloning the repository"
-msg_ok "Cloned Git repository"
+# git clone --quiet https://${GITHUB_USER}:${GITHUB_PAT}@github.com/${GITHUB_REPOSITORY} /srv || error_exit "Failed cloning the repository"
+# msg_ok "Cloned Git repository"
 
-# Ensure local Git uses the stored credentials
-cd /srv || error_exit "Failed to change directory to /srv"
+# # Ensure local Git uses the stored credentials
+# cd /srv || error_exit "Failed to change directory to /srv"
 
 
-git config credential.helper store
+# git config credential.helper store
 
 # Create Docker networks
 msg_info "Creating Docker networks"
@@ -64,7 +64,7 @@ for container in $containerlist; do
 done
 
 # Show the checklist
-START_CONTAINERS=$(whiptail --backtitle "Homelab setup" --title "Select containers to start" --checklist "\nChoose containers:" 30 58 20 "${options[@]}")
+START_CONTAINERS=$(whiptail --title "Select containers to start" --checklist "Choose containers:" 30 58 20 "${options[@]}")
 
 
 # Convert the quoted string into an array
