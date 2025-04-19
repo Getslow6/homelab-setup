709df00f0e58c3b97d816cdcfbeb999e7b354247
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-04-19 13:44:11 +0000

    Update parentheses

--- setup_devcontainer/create_devcontainer.sh
+++ setup_devcontainer/create_devcontainer.sh
@@ -5,5 +5,5 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/Proxmo
 # Load self defined support functions
 source <(curl -fsSL https://github.com/Getslow6/homelab-setup/raw/main/setup.func)
 
-CTID=$(get_CTID())
+CTID=$(get_CTID)
 lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https://github.com/Getslow6/homelab-setup/raw/main/setup_devcontainer/setup_devcontainer.sh)"
