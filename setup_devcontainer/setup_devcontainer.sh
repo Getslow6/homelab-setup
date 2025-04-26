60e1e9ee3aa0ada7204971dfab7d43e988566b36
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-04-26 20:09:38 +0000

    Update to devcontainer setup

--- setup_devcontainer/setup_devcontainer.sh
+++ setup_devcontainer/setup_devcontainer.sh
@@ -6,9 +6,6 @@ source <(curl -fsSL https://github.com/Getslow6/homelab-setup/raw/main/setup.fun
 # Ensure the script stops on errors
 set -e
 
-# Install required dependencies (based on Chat GPT's suggestion)
-apt install -y curl git nodejs npm bash unzip gnupg ca-certificates
-
 # Install Docker
 curl -fsSL https://get.docker.com | sh
 
@@ -17,24 +14,10 @@ systemctl enable docker
 systemctl start docker
 usermod -aG docker $USER
 
-# Make a config file for code-server
-mkdir -p ~/.config/code-server
-echo "bind-addr: 0.0.0.0:8080
-auth: password
-password: $(openssl rand -base64 12)
-cert: false" > ~/.config/code-server/config.yaml
+GITHUB_REPOSITORY=$(get_input  "Enter your (forked) Home Assistant Github repository" "GitHub repository" "Getslow6/core") || error_exit "Failed to get GitHub repository"
+git clone --quiet --branch master --single-branch --depth 1 https://github.com/${GITHUB_REPOSITORY} homeassistant || error_exit "Failed cloning the repository"
 
-# Download and install code-server
-curl -fsSL https://code-server.dev/install.sh | sh
+cd homeassistant
 
-GITHUB_REPOSITORY=$(get_input  "Enter your forked Home Assistant repository" "GitHub repository" "Getslow6/core") || error_exit "Failed to get GitHub repository"
-git clone "$GITHUB_REPOSITORY" homeassistant-dev
-cd homeassistant-dev
 
-systemctl enable --now code-server@$USER
-systemctl restart code-server@$USER
 
-echo "Setup complete!"
-echo "You can access code-server at http://<your-lxc-ip>:8080"
-echo "The password is stored in ~/.config/code-server/config.yaml"
-echo "For simplicity, this is the password: $(cat ~/.config/code-server/config.yaml | grep password | cut -d ' ' -f 2)"
