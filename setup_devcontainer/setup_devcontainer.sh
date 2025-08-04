dbccff076da47e1ef5fff78b3228a1f6c3be0d07
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-08-04 19:31:11 +0200

    Update setup_devcontainer.sh

--- setup_devcontainer/setup_devcontainer.sh
+++ setup_devcontainer/setup_devcontainer.sh
@@ -12,6 +12,13 @@ then
     curl -fsSL https://get.docker.com | sh
 fi
 
+# Install VS-Code
+sudo apt update
+sudo apt install software-properties-common apt-transport-https curl
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install code
 
 # Setup docker to auto start on boot
 systemctl enable docker
