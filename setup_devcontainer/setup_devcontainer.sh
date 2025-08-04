f7abd2493c6b5da873f3bdeb2d358b79b6b982a6
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-08-04 20:02:49 +0200

    Update setup_devcontainer.sh

--- setup_devcontainer/setup_devcontainer.sh
+++ setup_devcontainer/setup_devcontainer.sh
@@ -33,7 +33,7 @@ msg_ok "Updated SSH configuration"
 
 msg_info "Cloning Git repository"
 rm -rf /root/home-assistant
-git clone --quiet --branch master --single-branch --depth 1 https://github.com/${GITHUB_REPOSITORY} home-assistant || error_exit "Failed cloning the repository"
+git clone --quiet --branch dev --single-branch --depth 1 https://github.com/${GITHUB_REPOSITORY} home-assistant || error_exit "Failed cloning the repository"
 
 msg_ok "Cloned Git repository"
 
