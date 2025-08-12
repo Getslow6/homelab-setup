cca90bf67ec626cf858e5ff73357cb59eb4a15f2
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-08-12 20:25:01 +0200

    Update setup_devcontainer.sh

--- setup_devcontainer/setup_devcontainer.sh
+++ setup_devcontainer/setup_devcontainer.sh
@@ -20,10 +20,11 @@ apt install -y curl ca-certificates gnupg > /dev/null 2>&1
 msg_ok "Installed required dependencies"
 
 # Add NodeSource repository for Node.js LTS
-curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+
 
 # Install Node.js and npm
 msg_info "Installing Node.js..."
+curl -fsSL https://deb.nodesource.com/setup_lts.x | bash - &>/dev/null
 apt install -y nodejs > /dev/null 2>&1
 msg_ok "Node.js version $(node -v) installed"
 
@@ -65,30 +66,28 @@ systemctl enable docker
 systemctl start docker
 usermod -aG docker $USER
 
-# SSH_PUBKEY=$(get_input  "Enter your Public SSH key. You can get it by putting in the terminal: cat ~/.ssh/id_ed25519.pub" "Public key" "") || error_exit "Failed to get GitHub repository"
-HA_REPOSITORY=$(get_input  "Enter your (forked) Home Assistant Github repository" "GitHub repository" "home-assistant/core") || error_exit "Failed to get Home Assistant forked repository"
-CC_REPOSITORY=$(get_input  "Enter your custom component Github repository" "GitHub repository" "username/custom-component") || error_exit "Failed to get Custom Component repository"
+SSH_PUBKEY=$(get_input  "Enter your Public SSH key. You can get it by putting in the terminal: cat ~/.ssh/id_ed25519.pub" "Public key" "") || error_exit "Failed to get GitHub repository"
+HA_REPOSITORY=$(get_input  "Enter your (forked) Home Assistant Github repository" "GitHub repository" "Getslow6/core") || error_exit "Failed to get Home Assistant forked repository"
+CC_REPOSITORY=$(get_input  "Enter your custom component Github repository" "GitHub repository" "Getslow6/monitor_docker") || error_exit "Failed to get Custom Component repository"
 CC_FOLDER="${CC_REPOSITORY#*/}" # Cut away everything until the first /
 
 clear
 
 msg_info "Updating SSH configuration"
-
 echo "$SSH_PUBKEY" >> /root/.ssh/authorized_keys
-
 msg_ok "Updated SSH configuration"
 
 
 msg_info "Cloning Home Assistant Core"
 rm -rf /root/home-assistant
 git clone --quiet --single-branch --depth 1 https://github.com/${HA_REPOSITORY} /srv/home-assistant || error_exit "Failed cloning the Home Assistant repository"
+msg_ok "Cloned Home Assistant Core"
 
 msg_info "Cloning Custom component"
 git clone --quiet --single-branch --depth 1 https://github.com/${CC_REPOSITORY} /srv/${CC_FOLDER}|| error_exit "Failed cloning the Custom component repository"
-
+msg_ok "Cloned Custom component"
 
 msg_info "Updating devcontainer.json with custom component mount"
-
 DEVCONTAINER_JSON="/srv/home-assistant/.devcontainer/devcontainer.json"
 
 # Define the mount to add
