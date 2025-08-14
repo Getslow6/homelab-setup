f9a63c00740ad50731aaa9525f99f5d9379fbd7b
Getslow6 <43093176+Getslow6@users.noreply.github.com>
2025-08-14 20:09:14 +0000

    Remove devcontainer

--- setup_devcontainer/create_devcontainer.sh
+++
@@ -1,9 +0,0 @@
-
-# Use the Community helper script for an LXC container based on Debian:
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/ct/debian.sh)"
-
-# Load self defined support functions
-source <(curl -fsSL https://github.com/Getslow6/homelab-setup/raw/main/setup.func)
-
-CTID=$(get_CTID)
-lxc-attach -n "$CTID" -- bash -c "$(curl -fsSL https://github.com/Getslow6/homelab-setup/raw/main/setup_devcontainer/setup_devcontainer.sh)"
